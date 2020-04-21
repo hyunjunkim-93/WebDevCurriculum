bfc323657043a79b20db2aef5508ac33abba0003
hyunjun.kim <hyunjun.kim@knowre.com>
2020-04-21 11:34:08 +0900

    (JavaScript) Quest06 제출

--- Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -1,15 +1,45 @@
 class Desktop {
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	#icons = [];
+
+	constructor(...icons) {
+	}
+
+	render() {}
 };
 
 class Icon {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	#name;
+	#img;
+	#width;
+	#height
+
+	constructor(){}
+
+	move(){}
 };
 
 class Folder {
 	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	#name;
+	#img;
+	#width;
+	#height
+
+	constructor(){}
+
+	move(){}
+	open(){}
 };
 
 class Window {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	#name;
+	#content;
+
+	constructor(){}
+
+	move(){}
+	close(){}
 };
