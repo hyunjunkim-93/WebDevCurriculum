c177fc29351a79a92a01c9689321ad013b613075
hyunjun.kim <hyunjun.kim@knowre.com>
2020-05-18 15:56:22 +0900

    (Node.js) Quest12 과제 수행

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,3 +1,253 @@
 class Notepad {
+  #notes = []
+  #currentNote = {}
+  #targetIndex
+
 	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
-};
+  constructor() {
+  }
+
+  get notes() {
+    return this.#notes
+  }
+
+  get currentNote() {
+    return this.#currentNote
+  }
+
+  set notes(data) {
+    this.#notes.push(data)
+  }
+
+  set currentNote(data) {
+    this.#currentNote = data
+  }
+  
+  addNote(emptyNote) {
+    this.#currentNote = emptyNote
+    this.#notes.unshift(emptyNote)
+    this.addNoteAPI(emptyNote)
+  }
+
+  checkUpdateCondition() {
+    this.#targetIndex = this.findTargetIndex(this.#currentNote.created)
+    if (this.#notes[this.#targetIndex].title !== this.#currentNote.title
+      || this.#notes[this.#targetIndex].description !== this.#currentNote.description) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  updateNote() {
+    this.updateNoteAPI(this.#currentNote)
+    this.#notes[this.#targetIndex] = this.#currentNote
+    this.sortNotes()
+  }
+  
+  findTargetIndex(target) {
+    const targetNoteIndex = this.#notes.findIndex(el => {
+      return el.created === target
+    })
+    return targetNoteIndex
+  }
+
+  getNotesAPI() {
+    return fetch('http://localhost:8080/api/notepad')
+      .then(res => res.json())
+      .then(data => {
+        this.#notes = data
+        this.sortNotes()
+      })
+  }
+
+  sortNotes() {
+    this.#notes.sort((a, b) => {
+      const firstValue = a.modified.replace(/[^0-9]/g,"");
+      const secondValue = b.modified.replace(/[^0-9]/g,"");
+      return secondValue - firstValue
+    })
+  }
+
+  addNoteAPI(data) {
+    this.customFetch('POST', '/notepad', data)
+    this.sortNotes()
+  }
+
+  updateNoteAPI(data) {
+    this.customFetch('PUT', '/notepad', data)
+  }
+
+  deleteNoteAPI() {
+    this.customFetch('DELETE', '/notepad', this.#currentNote.created)
+  }
+
+  async customFetch(type = '', route = '', data = {}) {
+    const response = await fetch(`http://localhost:8080/api${route}`, {
+      method: type,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    return response.json()
+  }
+
+  getEmptyNote() {
+    const note = {
+      created: this.getCreated(),
+      modified: this.getModified(),
+      title: 'New Note',
+      description: '',
+    }
+    return note
+  }
+
+  getCurrentDate() {
+    const date = new Date()
+    const dateTimeFormat
+      = new Intl.DateTimeFormat
+        ('en',
+        { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: 'numeric' })
+    
+    const [
+      { value: month },,{ value: day },,{ value: year },,{ value: hour },,{ value: minute },,{ value: second }
+    ] = dateTimeFormat.formatToParts(date)
+    return { year, month, day, hour, minute, second }
+  }
+
+  getCreated() {
+    const { year, month, day, hour, minute, second } = this.getCurrentDate()
+    return `${year}${month}${day}-${hour}${minute}${second}`
+  }
+
+  getModified() {
+    const { year, month, day, hour, minute, second } = this.getCurrentDate()
+    return `${year}/${month}/${day} ${hour}:${minute}:${second}`
+  }
+}
+
+class ViewNotepad {
+  #title
+  #description
+  #date
+  #notepadList
+  #notepad
+  #aside
+  #newBtn
+  #deleteBtn
+  #currentEl
+
+  constructor(notepad) {
+    this.#notepadList = document.querySelector('.notepad-list')
+    this.#title = document.querySelector('.notepad-title')
+    this.#description = document.querySelector('.notepad-description')
+    this.#date = document.querySelector('.notepad-date')
+    this.#aside = document.querySelector('.notepad-aside')
+    this.#newBtn = document.querySelector('.notepad-btn--new')
+    this.#deleteBtn = document.querySelector('.notepad-btn--delete')
+    this.#notepad = notepad
+    this.render()
+    this.addEvents()
+  }
+
+  focusRecentNote() {
+    if (this.#notepadList.children[2]) {
+      this.#notepadList.children[2].click()
+      this.#description.focus()
+    }
+  }
+
+  render() {
+    this.#notepad.getNotesAPI().then(() => {
+      this.#notepad.notes.forEach(note => {
+        this.setNoteLi(note)
+      })
+      this.focusRecentNote()
+    })
+  }
+
+  updateView() {
+    this.#notepadList.removeChild(this.#currentEl)
+    const { li, title, date } = this.cloneLiTemplate()
+    this.setNoteLiContent(this.#notepad.notes[0], title, date)
+    this.addNoteLoadEvent(li, this.#notepad.notes[0])
+    this.#notepadList.insertBefore(li, this.#notepadList.children[2])
+  }
+
+  addEvents() {
+    this.addNoteUpdateEvent()
+    this.addNewNoteEvent()
+    this.deleteNoteEvent()
+  }
+
+  setNoteLi(note) {
+    const { li, title, date } = this.cloneLiTemplate()
+    this.setNoteLiContent(note, title, date)
+    this.appendNoteLi(li)
+    this.addNoteLoadEvent(li, note)
+  }
+
+  addEmptyNote() {
+    const emptyNote = this.#notepad.getEmptyNote()
+    this.#notepad.addNote(emptyNote)
+    this.setNoteLi(emptyNote)
+    this.focusRecentNote()
+  }
+
+  cloneLiTemplate() {
+    const template = document.querySelector('#template-notepad-list').content.cloneNode(true)
+    const li = template.querySelector('.notepad-li')
+    const title = li.querySelector('.li-title')
+    const date = li.querySelector('.li-date')
+    return { li, title, date }
+  }
+
+  setNoteLiContent(note, title, date) {
+    title.innerHTML = note.title
+    date.innerHTML = note.modified
+    this.#date.innerHTML = note.modified
+  }
+
+  appendNoteLi(li) {
+    this.#notepadList.appendChild(li)
+  }
+
+  addNoteLoadEvent(el, content) {
+    el.addEventListener('click', e => {
+      this.#notepad.currentNote = { ...content }
+      this.#date.innerHTML = this.#notepad.currentNote.modified
+      this.#title.value = this.#notepad.currentNote.title
+      this.#description.value = this.#notepad.currentNote.description
+      this.#currentEl = e.currentTarget
+    })
+  }
+  
+  addNoteUpdateEvent() {
+    this.#aside.addEventListener('click', () => {
+      const data = {
+        title: this.#title.value,
+        description: this.#description.value,
+        modified: this.#notepad.getModified(),
+      }
+      this.#notepad.currentNote = Object.assign(this.#notepad.currentNote, data)
+      if (this.#notepad.checkUpdateCondition()) {
+        this.#notepad.updateNote()
+        this.updateView()
+      }
+    })
+  }
+
+  addNewNoteEvent() {
+    this.#newBtn.addEventListener('click', () => {
+      this.addEmptyNote()
+    })
+  }
+
+  deleteNoteEvent() {
+    this.#deleteBtn.addEventListener('click', () => {
+      this.#notepadList.removeChild(this.#currentEl)
+      this.#notepad.deleteNoteAPI()
+    })
+  }
+}
--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -1,15 +1,118 @@
 const express = require('express'),
 	path = require('path'),
+	fs = require('fs'),
 	app = express();
 
 app.use(express.static('client'));
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	next()
+})
+app.use('/api/notepad', (req, res, next) => {
+	let data = ''
+	req.on('data', chunk => {
+		data += chunk
+	})
+	req.on('end', () => {
+		req.body = data.toString()
+		next()
+	})
+})
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
+	res.sendFile(path.join(__dirname, 'index.html'))
+})
 
 /* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
+// GET 요청을 받으면 폴더의 파일에 대한 데이터를 반환해준다
+app.get('/api/notepad', (req, res) => {
+	let result;
+	try {
+    const files = fs.readdirSync('./notepad')
+		result = files.reduce((acc, curr) => {
+      const data = fs.readFileSync(`./notepad/${curr}`).toString()
+      const created = curr.split('.')[0]
+			const { title, description, modified } = JSON.parse(data)
+			const el = { created, title, description, modified }
+			acc.push(el)
+			return acc
+    }, [])
+    res.end(JSON.stringify(result))
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			console.error('Files were not founded!')
+		} else {
+			throw err
+    }
+	}
+})
+
+// POST 요청을 받았을 때 바디의 데이터를 기준으로 파일을 생성한다
+app.post('/api/notepad', (req, res) => {
+  const { created, title, description, modified } = JSON.parse(req.body)
+  
+  try {
+    fs.accessSync(`./notepad/${created}.txt`, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK)
+    console.error('Already existed')
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      fs.writeFile(`./notepad/${created}.txt`, JSON.stringify({ title, description, modified }), err => {
+        if (err) throw err
+      })
+    } else {
+      console.error('Unknown Error!')
+    }
+  }
+})
+
+// PUT 요청을 받았을 때 바디의 데이터를 기준으로 파일을 업데이트 한다
+app.put(`/api/notepad`, (req, res) => {
+  const { created, title, description, modified } = JSON.parse(req.body)
+  const data = JSON.stringify({ title, description, modified })
+  
+  try {
+    fs.accessSync(`./notepad/${created}.txt`, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK)
+    const readStream = fs.createReadStream(`./notepad/${created}.txt`)
+    const writeStream = fs.createWriteStream(`./notepad/${created}.txt`)
+    readStream.pipe(writeStream)
+    writeStream.on('finish', () => {
+      console.log('파일 쓰기 완료')
+    })
+    writeStream.write(data)
+    writeStream.end()
+    res.end({ title, description, modified })
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.error('No File!')
+    } else {
+      console.error('Unknown Error!')
+    }
+  }
+})
+
+// DELETE 요청을 받았을 때 바디의 데이터를 기준으로 해당 파일을 삭제한다
+app.delete(`/api/notepad`, (req, res) => {
+  const created = JSON.parse(req.body)
+
+  try {
+    fs.accessSync(`./notepad/${created}.txt`, fs.constants.F_OK | fs.constants.R_OK | fs.constants.W_OK)
+    fs.unlink(`./notepad/${created}.txt`, err => {
+      if (err) {
+        console.error(err)
+      }
+    })
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.error('No File!')
+    } else {
+      console.error('Unknown Error!')
+    }
+  }
+})
 
 const server = app.listen(8080, () => {
-	console.log('Server started!');
+	console.log('Server started!')
 });
